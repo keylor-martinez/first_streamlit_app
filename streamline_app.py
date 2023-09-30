18e20eb739e2cca23d9f58c2dfd0e9aa62877b79
Keylor Martinez <80994827+keylor-martinez@users.noreply.github.com>
2023-09-30 14:38:57 -0600

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -33,14 +33,14 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
-try:  
+try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    streamlit.text('HERE')
+    streamlit.header("PRUEBA")
     #back_from_function = get_fruityvice_data(fruite_choice)
-    #streamlit.dataframe(back_from_function)   
+    #streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
