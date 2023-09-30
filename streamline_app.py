01b5831f28309acc5e99cd14cdb167b9ecfb3009
Keylor Martinez <80994827+keylor-martinez@users.noreply.github.com>
2023-09-30 14:28:23 -0600

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -39,8 +39,8 @@ try:
     streamlit.error('Please select a fruit to get information.')
   else:
     back_from_function = get_fruityvice_data(fruite_choice)
-    streamlit.dataframe(back_from_function)
-    #streamlit.text('HERE')
+    #streamlit.dataframe(back_from_function)
+    streamlit.text('HERE')
 except URLError as e:
   streamlit.error()
 
